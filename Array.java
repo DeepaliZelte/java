ff037a2363f33967b100cff3bb06bde123376340
DeepaliZelte <zeltedipali123@gmail.com>
2021-05-26 17:58:25 +0530

    this is 2nd demo

---
+++ Array.java
@@ -0,0 +1,20 @@
+class Palindrom
+{
+    public static void main(String arg[])
+    {
+     int p[][]= { { 3,4,5,6},
+                  {4,2,6,7},
+                   {8,1,2,4}
+                };
+ for(int i=0;i<4;i++)
+ {
+     for(int j=0;j<4;j++)
+     {
+         System.out.print(p[i][j] + " ");
+     }
+     System.out.println("");
+ }
+ 
+    }
+
+}
